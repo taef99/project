6f2275e6023a0940014ec8d696867fd25a07a164
taef <taeef098@gmail.com>
2021-07-26 19:59:08 +0300

    Add project files.

---
+++ teacher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Teacherdata
+{
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" ID :");
+            Console.WriteLine(" Name of Teacher :");
+            Console.WriteLine(" Name of the Class and Section :");
+
+            Console.ReadKey();
+
+        }
+    }
+}
